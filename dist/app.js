20d58d01dd50630394d609dd6c58e2535062afb4
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-10-19 14:14:16 -0400

    bugfix: ajuste e correções de erro

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,13 @@
+import morgan from "morgan";
 import express from "express";
 import { config } from "dotenv";
+import appRouter from "./routes/index.js";
 config();
 const app = express();
 // middlewares
 app.use(express.json());
+// remove it in production
+app.use(morgan("dev"));
+app.use("/api/v1", appRouter);
 export { app };
 //# sourceMappingURL=app.js.map
